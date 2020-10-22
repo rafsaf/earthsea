d154bc42a4c84777ae7a29e9e760de3e38f47c06
rafsaf <rafal.safin12@gmail.com>
2020-10-22 15:38:47 +0200

    gradient in parts

--- src/shared/Part.js
+++ src/shared/Part.js
@@ -7,6 +7,7 @@ export default function Part(props) {
     const height = props.height ? props.height : 400
     const color = props.color ? props.color : 'white'
     const background = props.background ? props.background : '#1C7C73'
+    const background2= 'linear-gradient(90deg, rgba(0,42,50,1) 0%, rgba(9,100,121,1) 66%, rgba(26,151,161,1) 100%)'
     const lg = props.lg ? props.lg : 'auto'
     const font = props.font ? props.font : 'Oswald, sans-serif'
     const left = props.left
@@ -19,6 +20,7 @@ export default function Part(props) {
             minHeight: height, 
             color: color,
             backgroundColor: background,
+            backgroundImage: background2,
             fontFamily: font,
         }}>
         <div className='d-none d-lg-block'>
