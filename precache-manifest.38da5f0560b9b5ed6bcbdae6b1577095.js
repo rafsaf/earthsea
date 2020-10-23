40403f248cf8b7c01b2cc8e3b534127f05684a2a
Rafał Safin <rafal.safin12@gmail.com>
2020-10-24 01:23:57 +0200

    Updates

---
+++ precache-manifest.38da5f0560b9b5ed6bcbdae6b1577095.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e043c51a8ba11a0af9749ebbe4b6e2",
+    "url": "/rafsaf/earthsea/index.html"
+  },
+  {
+    "revision": "90266a4dfee7c07c924c",
+    "url": "/rafsaf/earthsea/static/css/2.626e4169.chunk.css"
+  },
+  {
+    "revision": "8b5c92ebbce5666712eb",
+    "url": "/rafsaf/earthsea/static/css/main.895f3a92.chunk.css"
+  },
+  {
+    "revision": "90266a4dfee7c07c924c",
+    "url": "/rafsaf/earthsea/static/js/2.3333dfcc.chunk.js"
+  },
+  {
+    "revision": "559acfd77e922f29e649a03531e2264a",
+    "url": "/rafsaf/earthsea/static/js/2.3333dfcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5c92ebbce5666712eb",
+    "url": "/rafsaf/earthsea/static/js/main.c1001545.chunk.js"
+  },
+  {
+    "revision": "a8fbb8dcce66bb03ff1e",
+    "url": "/rafsaf/earthsea/static/js/runtime-main.106a661f.js"
+  },
+  {
+    "revision": "ba07da22f69f32d919a3729124ba0cfa",
+    "url": "/rafsaf/earthsea/static/media/img2.ba07da22.jpg"
+  },
+  {
+    "revision": "2a8cdbf48bc8321df0de1e72e0c4ede0",
+    "url": "/rafsaf/earthsea/static/media/img3.2a8cdbf4.jpg"
+  },
+  {
+    "revision": "171f21bc38683539d1cc41a068cc0f28",
+    "url": "/rafsaf/earthsea/static/media/img4.171f21bc.jpg"
+  },
+  {
+    "revision": "4cdf0c52d90c564dc47905428e7d513e",
+    "url": "/rafsaf/earthsea/static/media/ziemiomorze.4cdf0c52.jpg"
+  }
+]);
