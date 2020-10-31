105119564d240dc3a1a2a7340cb68333fb3352ce
Rafał Safin <rafal.safin12@gmail.com>
2020-10-31 09:31:55 +0100

    fake.js

--- src/fake.js
+++ src/fake.js
@@ -35,4 +35,144 @@ export default
         "unlike": "0",
         
     },
+    {
+        "id" : 1,
+
+        "image_confirm": true,
+
+        "main_page": true,
+
+        "title": "Ged",
+        
+        "slug": "Ged",
+
+        "description": "Jeden z głownych bohaterów cyklu, potężny czarnoksiężnik, późniejszy Arcymag Ziemiomorza oraz Władca Smoków znany jako Krogulec lub Jastrząb, pochodził z wyspy Gont.",
+
+        "author": "Rafał Safin",
+
+        "created": "2020-07-31",
+
+        "image": "https://2.bp.blogspot.com/-3LptOEumhCo/Uz_OnDyTNLI/AAAAAAAABPs/vcwO0x9pJbY/s1600/czarnoksieznik-z-archipelagu-b-iext22043259.jpg",
+
+        "source": "https://2.bp.blogspot.com",
+
+        // 50-160 chars
+
+        "versions": [
+            150,
+            151,
+            152,
+            153,
+        ],
+
+        "like": "10",
+
+        "unlike": "0",
+        
+    },
+    {
+        "id" : 1,
+
+        "image_confirm": true,
+
+        "main_page": true,
+
+        "title": "Ged",
+        
+        "slug": "Ged",
+
+        "description": "Jeden z głownych bohaterów cyklu, potężny czarnoksiężnik, późniejszy Arcymag Ziemiomorza oraz Władca Smoków znany jako Krogulec lub Jastrząb, pochodził z wyspy Gont.",
+
+        "author": "Rafał Safin",
+
+        "created": "2020-07-31",
+
+        "image": "https://2.bp.blogspot.com/-3LptOEumhCo/Uz_OnDyTNLI/AAAAAAAABPs/vcwO0x9pJbY/s1600/czarnoksieznik-z-archipelagu-b-iext22043259.jpg",
+
+        "source": "https://2.bp.blogspot.com",
+
+        // 50-160 chars
+
+        "versions": [
+            150,
+            151,
+            152,
+            153,
+        ],
+
+        "like": "10",
+
+        "unlike": "0",
+        
+    },
+    {
+        "id" : 1,
+
+        "image_confirm": true,
+
+        "main_page": true,
+
+        "title": "Ged",
+        
+        "slug": "Ged",
+
+        "description": "Jeden z głownych bohaterów cyklu, potężny czarnoksiężnik, późniejszy Arcymag Ziemiomorza oraz Władca Smoków znany jako Krogulec lub Jastrząb, pochodził z wyspy Gont.",
+
+        "author": "Rafał Safin",
+
+        "created": "2020-07-31",
+
+        "image": "https://2.bp.blogspot.com/-3LptOEumhCo/Uz_OnDyTNLI/AAAAAAAABPs/vcwO0x9pJbY/s1600/czarnoksieznik-z-archipelagu-b-iext22043259.jpg",
+
+        "source": "https://2.bp.blogspot.com",
+
+        // 50-160 chars
+
+        "versions": [
+            150,
+            151,
+            152,
+            153,
+        ],
+
+        "like": "10",
+
+        "unlike": "0",
+        
+    },
+    {
+        "id" : 1,
+
+        "image_confirm": true,
+
+        "main_page": true,
+
+        "title": "Ged",
+        
+        "slug": "Ged",
+
+        "description": "Jeden z głownych bohaterów cyklu, potężny czarnoksiężnik, późniejszy Arcymag Ziemiomorza oraz Władca Smoków znany jako Krogulec lub Jastrząb, pochodził z wyspy Gont.",
+
+        "author": "Rafał Safin",
+
+        "created": "2020-07-31",
+
+        "image": "https://2.bp.blogspot.com/-3LptOEumhCo/Uz_OnDyTNLI/AAAAAAAABPs/vcwO0x9pJbY/s1600/czarnoksieznik-z-archipelagu-b-iext22043259.jpg",
+
+        "source": "https://2.bp.blogspot.com",
+
+        // 50-160 chars
+
+        "versions": [
+            150,
+            151,
+            152,
+            153,
+        ],
+
+        "like": "10",
+
+        "unlike": "0",
+        
+    },
 ]
